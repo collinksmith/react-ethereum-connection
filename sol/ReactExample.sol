f00118720bdf364837889bed24f7a9cb5967c503
Collin Smith <collin@collinsmith.me>
2017-12-13 22:51:47 -0600

    TEMP - Add solidity code

---
+++ sol/ReactExample.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.11;
+
+contract ReactExample {
+    address private owner;
+    string public you_awesome;
+    string private secret;
+    string private state;
+    
+    function ReactExample () public {
+        owner = msg.sender;
+        you_awesome = "You're awesome";
+        secret = "secret data";
+        state = "initial state";
+    }
+    
+    function getSecret () public view returns (string) {
+        return secret;
+    }
+    
+    function getState () public view returns (string) {
+        return state;    
+    }
+    
+    function setState (string newState) public payable {
+        state = newState;
+    }
+    
+    function kill () public {
+        require (msg.sender == owner);
+        selfdestruct (owner);
+    }
+    
+    function () public payable {
+        revert ();
+    }
+}
